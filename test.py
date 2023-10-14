fc9fa35f1291df446b5fb91c3d9cf17e0a01dfbc
James jpb <jpbefx@hotmail.com>
2023-10-14 15:42:55 -0500

    Added LinearRegressionTUTUORIAL_FILE.py and RegressionWorkingFileTutorial.py

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 import tensorflow
+#import tensorflow.python.platform.build_info as build
+import keras
+from tensorflow.python.platform import build_info as build
 
-import karas
+print(build.build_info['cuda_version'])
+
+print()
+
+print(build.build_info)
+
+#print(tf.test.is_built_with_cuda())
+print()
+
+print(build.build_info['cuda_version'])
+
+print()
+
+print(build.build_info['cudnn_version'])
